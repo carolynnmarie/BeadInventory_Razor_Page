a11ea280c5b0c3506f2c79b3b3b2743f47f2c168
Carolynn Vansant <carolynnmarie@gmail.com>
2018-11-10 15:21:35 -0500

    added to SeedData class

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -24,7 +24,9 @@ namespace BeadDotNetApp.Models{
                 new Bead{Material = "Adventurine",Shape = "round", Color = "green", SizeMM = 6,
                         Quantity = 20,PricePoint = 0.03M,Brand = "Bead Gallery"},
                 new Bead{Material = "Adventurine",Shape = "round", Color = "green", SizeMM = 4,
-                        Quantity = 25,PricePoint = 0.03M,Brand = "Bead Gallery"}
+                        Quantity = 25,PricePoint = 0.03M,Brand = "Bead Gallery"},
+                new Bead{Material= "Hematite",Shape="donut",Color="metallic grey", SizeMM = 12,
+                        Quantity = 15, PricePoint = 0.1M, Brand = "Bead Gallery"}
                 );
                 context.SaveChanges();
             };
